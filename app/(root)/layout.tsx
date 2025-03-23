ecd90b5c71acbf63d0c7b64ccafdf5b399c3ba46
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-03-23 12:03:50 +0530

    master: implement the header

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
 import React, { ReactNode } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="root-layout">
+      <nav>
+        <Link href="/" className="flex flex-row items-center gap-2">
+          <Image src={"/logo.svg"} width={38} height={32} alt="logo" />
+          <h2 className="text-primary-100">PrepWise</h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
+  );
 };
 
 export default RootLayout;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Page = () => {
-  return <div>Home Page</div>;
+  return (
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice on real interview questions & get instant feedback
+          </p>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Page;
