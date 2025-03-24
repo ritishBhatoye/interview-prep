da9ba382d25f5a0d410ee705e42f6f1b41fb0491
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-03-24 23:34:42 +0530

    master : user login

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,14 @@
 "use client";
 
+import { isAuthenticated } from "@/lib/actions/auth.action";
+import { redirect } from "next/navigation";
 import React, { ReactNode } from "react";
 
-const AuthLayout = ({ children }: { children: ReactNode }) => {
+const AuthLayout = async ({ children }: { children: ReactNode }) => {
+  const isUserAuthenticated: boolean = await isAuthenticated();
+
+  if (isUserAuthenticated) redirect("/");
+
   return <div className="auth-layout">{children}</div>;
 };
 
