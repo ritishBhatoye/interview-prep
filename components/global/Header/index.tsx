83f9474fb31a0b1b707e8344eec533a29aaef7c8
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-03-26 22:41:42 +0530

    JOB-PORTAL-TASK:1 header implementation

---
+++ components/global/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="w-11/12 2xl:w-9/12 mx-auto">
+      <ul className="flex flex-row list-none gap-5">
+        <li></li>
+      </ul>
+    </div>
+  );
+}
---
+++ helpers/index.ts
@@ -0,0 +1,27 @@
+const headerData: any = [
+  {
+    id: 1,
+    label: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    label: "About",
+    link: "/about",
+  },
+  {
+    id: 1,
+    label: "Home",
+    link: "/",
+  },
+  {
+    id: 1,
+    label: "Home",
+    link: "/",
+  },
+  {
+    id: 1,
+    label: "Home",
+    link: "/",
+  },
+];
