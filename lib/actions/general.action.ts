449114819b154e3d1b0887d373b6fcb14e61401b
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-03-29 20:49:02 +0530

    few updates

--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -1,9 +1,9 @@
-// "use server";
+"use server";
 
 // import { generateObject } from "ai";
 // import { google } from "@ai-sdk/google";
 
-// import { db } from "@/firebase/admin";
+import { db } from "@/firebase/admin";
 // import { feedbackSchema } from "@/constants";
 
 // export async function createFeedback(params: CreateFeedbackParams) {
@@ -66,60 +66,60 @@
 //   }
 // }
 
-// export async function getInterviewById(id: string): Promise<Interview | null> {
-//   const interview = await db.collection("interviews").doc(id).get();
+export async function getInterviewById(id: string): Promise<Interview | null> {
+  const interview = await db.collection("interviews").doc(id).get();
 
-//   return interview.data() as Interview | null;
-// }
+  return interview.data() as Interview | null;
+}
 
-// export async function getFeedbackByInterviewId(
-//   params: GetFeedbackByInterviewIdParams
-// ): Promise<Feedback | null> {
-//   const { interviewId, userId } = params;
+export async function getFeedbackByInterviewId(
+  params: GetFeedbackByInterviewIdParams
+): Promise<Feedback | null> {
+  const { interviewId, userId } = params;
 
-//   const querySnapshot = await db
-//     .collection("feedback")
-//     .where("interviewId", "==", interviewId)
-//     .where("userId", "==", userId)
-//     .limit(1)
-//     .get();
+  const querySnapshot = await db
+    .collection("feedback")
+    .where("interviewId", "==", interviewId)
+    .where("userId", "==", userId)
+    .limit(1)
+    .get();
 
-//   if (querySnapshot.empty) return null;
+  if (querySnapshot.empty) return null;
 
-//   const feedbackDoc = querySnapshot.docs[0];
-//   return { id: feedbackDoc.id, ...feedbackDoc.data() } as Feedback;
-// }
+  const feedbackDoc = querySnapshot.docs[0];
+  return { id: feedbackDoc.id, ...feedbackDoc.data() } as Feedback;
+}
 
-// export async function getLatestInterviews(
-//   params: GetLatestInterviewsParams
-// ): Promise<Interview[] | null> {
-//   const { userId, limit = 20 } = params;
+export async function getLatestInterviews(
+  params: GetLatestInterviewsParams
+): Promise<Interview[] | null> {
+  const { userId, limit = 20 } = params;
 
-//   const interviews = await db
-//     .collection("interviews")
-//     .orderBy("createdAt", "desc")
-//     .where("finalized", "==", true)
-//     .where("userId", "!=", userId)
-//     .limit(limit)
-//     .get();
+  const interviews = await db
+    .collection("interviews")
+    .orderBy("createdAt", "desc")
+    .where("finalized", "==", true)
+    .where("userId", "!=", userId)
+    .limit(limit)
+    .get();
 
-//   return interviews.docs.map((doc) => ({
-//     id: doc.id,
-//     ...doc.data(),
-//   })) as Interview[];
-// }
+  return interviews.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as Interview[];
+}
 
-// export async function getInterviewsByUserId(
-//   userId: string
-// ): Promise<Interview[] | null> {
-//   const interviews = await db
-//     .collection("interviews")
-//     .where("userId", "==", userId)
-//     .orderBy("createdAt", "desc")
-//     .get();
+export async function getInterviewsByUserId(
+  userId: string
+): Promise<Interview[] | null> {
+  const interviews = await db
+    .collection("interviews")
+    .where("userId", "==", userId)
+    .orderBy("createdAt", "desc")
+    .get();
 
-//   return interviews.docs.map((doc) => ({
-//     id: doc.id,
-//     ...doc.data(),
-//   })) as Interview[];
-// }
+  return interviews.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as Interview[];
+}
