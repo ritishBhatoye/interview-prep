1dbb3332f71f87d397c636ce34e361156f013bda
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-04-22 22:23:43 +0530

    Enhance SearchHeader component layout by adding icons for location and job type, improving user interaction.

--- components/searchJob/SearchHeader/index.tsx
+++ components/searchJob/SearchHeader/index.tsx
@@ -1,12 +1,18 @@
 import { Input } from "@/components/ui/input";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { BriefcaseIcon, MagnifyingGlassIcon, MapPinIcon } from "@heroicons/react/24/outline";
 import React from "react";
 
 const SearchHeader = () => {
   return (
-    <div className="">
-      <MagnifyingGlassIcon className="w-5 h-4" />
-      <Input className="border-0" placeholder="Search typing your job search input" />
+    <div className="flex flex-row items-center justify-between">
+      <div className="flex flex-row items-center">
+        <MagnifyingGlassIcon className="w-5 h-4" />
+        <Input className="border-0" placeholder="Search typing your job search input" />
+      </div>
+      <div className="flex flex-row items-center">
+        <MapPinIcon className="w-5 h-5 border border-white rounded-full cursor-pointer hover:bg-opacity-50" />
+        <BriefcaseIcon className="w-5 h-5 border border-white rounded-full cursor-pointer hover:bg-opacity-50" />
+      </div>
     </div>
   );
 };
