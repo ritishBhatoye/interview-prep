c77ca790ff2521ac29fd2d00d4ee60f7dde43712
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-05-12 22:04:48 +0530

    Implement hover state for logout button in Header component to improve user interaction and visual feedback

--- components/global/Header/index.tsx
+++ components/global/Header/index.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import { headerData } from "@/helpers";
 import { HeaderDataTypeProps, User } from "@/types";
 import UserIcon from "@heroicons/react/24/solid/UserIcon";
+import { Button } from "@/components/ui/button";
 
 interface HeaderProps {
   user: User | null;
@@ -14,7 +15,8 @@ interface HeaderProps {
 
 export default function Header({ user }: HeaderProps) {
   const [isSelectTab, setIsSelectTab] = useState(1);
-  const [isLogButton, setIsLogButton] = useState(false);
+  const [isLogButton, setIsLogButton]: [boolean, React.Dispatch<React.SetStateAction<boolean>>] =
+    useState(false);
   return (
     <div className="mx-auto py-10 w-10/12 2xl:w-9/12 max-w-screen-2xl">
       <nav className="flex flex-row items-center list-none gap-12 bg-white/8 hover:bg-white/10 rounded-4xl p-4">
@@ -36,10 +38,21 @@ export default function Header({ user }: HeaderProps) {
           ))}
         </ul>
         {user && (
-          <div className="ml-auto flex items-center gap-3 hover:gap-4 cursor-pointer shadow-md hover:bg-opacity-40 rounded-4xl bg-black p-4 transition transform hover:scale-110 duration-500 ">
-            <UserIcon className="w-6 h-5" scale={0.2} />
+          <div
+            onMouseEnter={() => setIsLogButton(true)}
+            onMouseLeave={() => setIsLogButton(false)}
+            className="ml-auto flex flex-col items-center gap-2 overflow-y-hidden"
+          >
+            <div className=" flex items-center gap-3 hover:gap-4 cursor-pointer shadow-md hover:bg-opacity-40 rounded-4xl bg-black p-4 transition transform hover:scale-110 duration-500 ">
+              <UserIcon className="w-6 h-5" scale={0.2} />
 
-            <p className="text-primary-100 font-medium underline">{user.name}</p>
+              <p className="text-primary-100 font-medium underline">{user.name}</p>
+            </div>
+            {isLogButton && (
+              <Button className="relative cursor-pointer p-4 px-6 rounded-4xl bg-cosmic-purple-200">
+                Logout
+              </Button>
+            )}
           </div>
         )}
       </nav>
