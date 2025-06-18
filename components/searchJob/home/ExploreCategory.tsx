ae3032ae4d8b2ac0f2d9a5dca4c647f6d0bf2edd
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-06-18 12:57:52 +0530

    Update VSCode settings to enhance development experience with new themes, formatter configurations, and Tailwind CSS IntelliSense support. Adjust ExploreCategory component layout for improved spacing consistency.

--- components/searchJob/home/ExploreCategory.tsx
+++ components/searchJob/home/ExploreCategory.tsx
@@ -13,7 +13,7 @@ const ExploreCategory = () => {
   return (
     <div className="grid grid-1 gap-12 py-5 mt-5 md:mt-6 lg:mt-7">
       <ExploreHeader />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 ">
         {exploreCategoryData.map((item) => (
           <ExploreCategoriesCard
             onClick={() => {
